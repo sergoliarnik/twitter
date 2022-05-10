b2162894af9764624014adcc6dc643f233f4b281
Serhiy Oliyarnik <sergoliarnik@gmail.com>
2022-05-10 14:41:41 +0300

    Only owner can chane or remove post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "admin", password: "123"
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
